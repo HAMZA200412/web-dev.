5df6c1b9d83fb714ab9d811e1e59827b52d3f6f8
Hamza Amoussa <146017448+HAMZA200412@users.noreply.github.com>
2024-06-11 16:33:33 +0000

    added job

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location":"Casablaca",
+        "salary":"200000 dhs",
+    },
+    {
+        "id": 2,
+        "title": "Data Scientist",
+        "location":"Rabat",
+        "salary":"300000 dhs",        
+    },
+    {
+        "id": 3,
+        "title": "Software engineer",
+        "location":"Rabat",        
+    }   
+]
+
 @app.route("/")
+
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",
+                           jobs=JOBS,
+                           company_name="Hamza")
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
